ec5b6724409a9a4aa8cf8a43d7b026a5bf265be8
JC-kip <jjcasasm16@gmail.com>
2024-08-04 01:08:03 +0000

    JC-ExcuseDone

--- src/app.js
+++ src/app.js
@@ -6,6 +6,23 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let who = ["The dog", "My Grandpa", "The weather", "A burglar"];
+  let action = ["ate", "threw away", "broke", "stole"];
+  let what = ["my homework", "my laptop", "the car"];
+  let when = ["yesterday!", "this morning!", "last week!", "during my lunch!"];
+
+  function selectArrayElement(arrayName, arrayLength) {
+    let randomNumber = Math.floor(Math.random() * arrayLength);
+    let selectedElement = arrayName[randomNumber];
+    return selectedElement;
+  }
+  let randomWho = selectArrayElement(who, who.length);
+  let randomAction = selectArrayElement(action, action.length);
+  let randomWhat = selectArrayElement(what, what.length);
+  let randomWhen = selectArrayElement(when, when.length);
+
+  let excuse = `${randomWho} ${randomAction} ${randomWhat} ${randomWhen}`;
+
+  const htmlElement = document.querySelector("#excuse");
+  htmlElement.innerHTML = excuse;
 };
